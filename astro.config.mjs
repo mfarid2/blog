630a6680c5793d27c3a0baa449a4c603a893122b
mfarid2 <m.faridothman2@gmail.com>
2026-04-07 22:15:29 +0800

    Completed Astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { searchForWorkspaceRoot } from 'vite';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mfarid2-blog.netlify.app"
+  site: "https://mfarid2-blog.netlify.app",
+  integrations: [preact()],
+  vite: {
+    server: {
+      fs: {
+        allow: [
+          searchForWorkspaceRoot(process.cwd()),
+          process.env.PNPM_HOME || '',
+        ],
+      },
+    },
+  },
 });
--- src/blog/post-4.md
+++ src/blog/post-4.md
@@ -1,5 +1,4 @@
 ---
-layout: ../../layouts/MarkdownPostLayouts.astro
 title: My Fourth Blog Post
 author: Astro Learner
 description: "This post will show on its own!"
